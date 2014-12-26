ff38af86c7a01bd98a217135f3a50d2e757e6c2f
Taylor Ren <taylor.ren@gmail.com>
2014-12-26 21:42:30 +0800

    `div` function done. `phi` function (like `atan2` function) done. Test samples done.

--- main.cpp
+++ main.cpp
@@ -37,6 +37,34 @@ public:
         return (double) sqrt(r * r + i * i);
     }
 
+    Php::Value phi() const {
+        const double PI=3.1415926;
+        const double EPS=1e-12;
+        if(i*i<EPS) // Falls on X axis
+        {
+            if(r*r <EPS) // X is also 0
+            {
+                throw Php::Exception("Undefined");
+            }
+            else if (r>0)
+                return 0;
+            else 
+                return PI;
+        }
+        
+        if(r*r<EPS) //Falls on Y axis
+        {
+            if(i*i<EPS)
+                throw Php::Exception("Undefined");
+            else if (i>0)
+                return PI/2;
+            else
+                return -PI/2;
+        }
+        
+        return atan2(i, r);
+    }
+
     Php::Value __toString() {
         std::ostringstream os;
         os << r;
@@ -75,28 +103,36 @@ public:
         double tr = r * (double) (a->getReal()) - i * (double) (a->getImage());
         double ti = i * (double) (a->getReal()) + r * (double) (a->getImage());
 
-        r=tr;
-        i=ti;
+        r = tr;
+        i = ti;
         return this;
     }
-    
-    Php::Value div(Php::Parameters &params)
-    {
-        Php::Value t=params[0];
-        Complex *b=(Complex*)t.implementation();
-        
-        
-        
+
+    Php::Value div(Php::Parameters &params) {
+        Php::Value t = params[0];
+        Complex *b = (Complex*) t.implementation();
+
+        double t1 = b->mod() * b->mod();
+
+        if (t1 < 1E-9)
+            throw Php::Exception("Division by zero");
+
+        double tr = r * (double) (b->getReal()) + i * (double) (b->getImage());
+        double ti = i * (double) (b->getReal()) - r * (double) (b->getImage());
+
+        r = tr / t1;
+        i = ti / t1;
+
+        return this;
     }
-    
-    Php::Value conjugate()
-    {
-        Complex *t=new Complex();
-        
-        t->r=r;
-        t->i=-i;
-        
-        return Php::Object("Complex", t);
+
+    Php::Value conjugate() {
+        Complex *t = new Complex();
+
+        t->r = r;
+        t->i = -i;
+
+        return Php::Object("tr\\Complex", t);
     }
 };
 
@@ -117,37 +153,38 @@ extern "C" {
         // for the entire duration of the process (that's why it's static)
         static Php::Extension extension("complex", "0.0.1");
 
-        //Php::Namespace myNamespace("trComplex");
+        Php::Namespace myNamespace("tr");
         Php::Class<Complex> complex("Complex");
 
         complex.method("mod", &Complex::mod,{});
+        complex.method("phi", &Complex::phi,{});
+        
         complex.method("real", &Complex::getReal,{});
         complex.method("image", &Complex::getImage,{});
+        
         complex.method("__construct", &Complex::__construct);
+        
         complex.method("add", &Complex::add,{
-            Php::ByVal("op", "Complex", false, true)
+            Php::ByVal("op", "tr\\Complex", false, true)
         });
         complex.method("sub", &Complex::sub,{
-            Php::ByVal("op", "Complex", false, true)
+            Php::ByVal("op", "tr\\Complex", false, true)
         });
         complex.method("mul", &Complex::mul,{
-            Php::ByVal("op", "Complex", false, true)
+            Php::ByVal("op", "tr\\Complex", false, true)
         });
         complex.method("div", &Complex::div,{
-            Php::ByVal("op", "Complex", false, true)
+            Php::ByVal("op", "tr\\Complex", false, true)
         });
+        
         complex.method("conjugate", &Complex::conjugate,{});
-        
-        
+
         complex.method("__toString", &Complex::__toString);
 
+        myNamespace.add(std::move(complex));
 
-
-
-        //myNamespace.add(std::move(complex));
-
-        //extension.add(std::move(myNamespace));
-        extension.add(std::move(complex));
+        extension.add(std::move(myNamespace));
+        //extension.add(std::move(complex));
 
         // return the extension
         return extension;
--- test/testComplex.php
+++ test/testComplex.php
@@ -1,28 +1,28 @@
 <?php
 
-use Complex;
 
-$c=new Complex(-3,-4);
+
+$c=new tr\Complex(-3,-4);
 echo "Mod of $c is: ".$c->mod()."\n";
 
-$d=new Complex(4,3);
+$d=new tr\Complex(4,3);
 echo "Mod of $d is: ".$d->mod()."\n";
 
-$e=new Complex(-4,-3);
+$e=new tr\Complex(-4,-3);
 echo ((string)$e."\n");
 echo "=============================\n";
 
-$a=new Complex(1,1);
-$b=new Complex(1,2);
+$a=new tr\Complex(1,1);
+$b=new tr\Complex(1,2);
 
-$res=new Complex();
+$res=new tr\Complex();
 echo ($res)."\n";
 $res=$a->add($b)->sub($c)->add($d);
 echo ($res)."\n";
 echo "=============================\n";
 
-$a=new Complex(1,2);
-$b=new Complex(3,4);
+$a=new tr\Complex(1,2);
+$b=new tr\Complex(3,4);
 $res=$a->mul($b)->add($c);
 
 echo $res."\n";
@@ -33,7 +33,60 @@ echo "=============================\n";
 echo ($b->add($b->conjugate()))."\n";
 echo "=============================\n";
 
+$a=new tr\Complex(1,2);
+$c=new tr\Complex(1,2);
+$res=$a->div($c);
 
+echo $res."\n";
+echo "After division a becomes: $a"."\n";
+echo "=============================\n";
 
+$a=new tr\Complex(1,2);
+$c=new tr\Complex();
 
+try
+{
+    $res=$a->div($c);
+}
+catch(Exception $e)
+{
+    echo "Caught exception.\n";
+}
+echo "=============================\n";
+
+$pi=3.1415926;
+
+$a=new tr\Complex(1,0);
+echo "$a phi is: ".$a->phi()*180/$pi."\n";
+
+$a=new tr\Complex(0,1);
+echo "$a phi is: ".$a->phi()*180/$pi."\n";
+
+$a=new tr\Complex(-1,0);
+echo "$a phi is: ".$a->phi()*180/$pi."\n";
+
+$a=new tr\Complex(0,-1);
+echo "$a phi is: ".$a->phi()*180/$pi."\n";
+
+$a=new tr\Complex(0,0);
+try
+{
+    echo "$a phi is: ".$a->phi()*180/$pi."\n";
+}
+catch (Exception $e)
+{
+    echo "Caught exception: $e\n";
+}
+
+$a=new tr\Complex(1,1);
+echo "$a phi is: ".$a->phi()*180/$pi."\n";
+
+$a=new tr\Complex(-1,1);
+echo "$a phi is: ".$a->phi()*180/$pi."\n";
+
+$a=new tr\Complex(-1,-1);
+echo "$a phi is: ".$a->phi()*180/$pi."\n";
+
+$a=new tr\Complex(1,-1);
+echo "$a phi is: ".$a->phi()*180/$pi."\n";
 
